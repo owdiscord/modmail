15b4769708a50ff0505478a368acae2d90ecf8ab
graphiteisaac <isaac@grphcrtv.com>
2026-01-11 21:49:09 +1100

    fix migration

--- src/data/migrations/20201021232940_create_moderator_role_overrides_table.js
+++ src/data/migrations/20201021232940_create_moderator_role_overrides_table.js
@@ -1,16 +1,16 @@
-exports.up = async function(knex, Promise) {
-  if (! await knex.schema.hasTable("moderator_role_overrides")) {
-    await knex.schema.createTable("moderator_role_overrides", table => {
+exports.up = async function (knex, Promise) {
+  if (!(await knex.schema.hasTable("moderator_role_overrides"))) {
+    await knex.schema.createTable("moderator_role_overrides", (table) => {
       table.string("moderator_id", 20);
-      table.string("thread_id", 36).nullable().defaultTo(null);
+      table.string("thread_id", 36).nullable().defaultTo(null).unique();
       table.string("role_id", 20);
 
-      table.primary(["moderator_id", "thread_id"]);
+      table.primary(["moderator_id"]);
     });
   }
 };
 
-exports.down = async function(knex, Promise) {
+exports.down = async function (knex, Promise) {
   if (await knex.schema.hasTable("moderator_role_overrides")) {
     await knex.schema.dropTable("moderator_role_overrides");
   }
