cb2cd965a0b8e19541e50d4fe08a1feea6e2bad8
Isaac <isaac@grphcrtv.com>
2026-01-30 09:30:44 +1100

    foolish is the man who doesn't :w

--- src/plugins/alert.ts
+++ src/plugins/alert.ts
@@ -2,24 +2,24 @@ import type { ModuleProps } from "../plugins";
 import { Emoji } from "../style";
 
 export default ({ config, commands }: ModuleProps) => {
-	commands.addInboxThreadCommand(
-		"alert",
-		"[opt:string]",
-		async (msg, args, thread) => {
-			if (!thread) return;
+  commands.addInboxThreadCommand(
+    "alert",
+    "[opt:string]",
+    async (msg, args, thread) => {
+      if (!thread) return;
 
-			if (args.opt && (args.opt as string).startsWith("c")) {
-				await thread.removeAlert(msg.author.id);
-				await thread.postSystemMessage(
-					`${Emoji.CheckBadge} Cancelled new message alert`,
-				);
-			} else {
-				await thread.addAlert(msg.author.id);
-				await thread.postSystemMessage(
-					`${Emoji.Alert} Pinging ${msg.member?.nickname || config.useDisplaynames ? msg.author.globalName || msg.author.username : msg.author.username} when this thread gets a new reply`,
-				);
-			}
-		},
-		{ allowSuspended: true },
-	);
+      if (args.opt && (args.opt as string).startsWith("c")) {
+        await thread.removeAlert(msg.author.id);
+        await thread.postSystemMessage(
+          `${Emoji.CheckBadge} Cancelled new message alert`,
+        );
+      } else {
+        await thread.addAlert(msg.author.id);
+        await thread.postSystemMessage(
+          `${Emoji.Schedule} Pinging ${msg.member?.nickname || config.useDisplaynames ? msg.author.globalName || msg.author.username : msg.author.username} when this thread gets a new reply`,
+        );
+      }
+    },
+    { allowSuspended: true },
+  );
 };
