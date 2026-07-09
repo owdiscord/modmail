3abe03f0eebef63be74e5736bc768408941f5d20
graphiteisaac <isaac@grphcrtv.com>
2026-07-09 22:18:50 +1000

    oops, properly fix

--- src/data/logs.ts
+++ src/data/logs.ts
@@ -97,10 +97,10 @@ export const formatLog = async (
       } else if (message.message_type === ThreadMessageType.Chat) {
         line += ` [CHAT] [${registeredName ? registeredName : message.user_name}] ${message.body}`;
         if (
-          message.metadata.attachments &&
+          message.metadata?.attachments &&
           typeof message.metadata.attachments === "object"
         )
-          line += `${message.body.length > 0 && message.metadata.attachments ? "\n" : ""}${(message.metadata.attachments as Array<string>).join("\n")}`;
+          line += `${message.body.length > 0 && message.metadata?.attachments ? "\n" : ""}${(message.metadata.attachments as Array<string>).join("\n")}`;
       } else if (message.message_type === ThreadMessageType.Command) {
         line += ` [COMMAND] [${registeredName ? registeredName : message.user_name}] ${message.body}`;
       } else if (message.message_type === ThreadMessageType.ReplyEdited) {
